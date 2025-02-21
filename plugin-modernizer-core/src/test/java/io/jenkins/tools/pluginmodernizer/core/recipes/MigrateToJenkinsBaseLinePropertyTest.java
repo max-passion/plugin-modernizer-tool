c3c06e53f6ca9d6797272a12cdb35bc86ce13199
max-passion <liam46funkvzo@hotmail.com>
2025-02-21 17:15:34 +0100

    fix(recipes): update Jenkins version in MigrateToJenkinsBaseLinePropertyTest to 2.479.3

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/MigrateToJenkinsBaseLinePropertyTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/MigrateToJenkinsBaseLinePropertyTest.java
@@ -428,7 +428,7 @@ public class MigrateToJenkinsBaseLinePropertyTest implements RewriteTest {
                   <properties>
                     <!-- https://www.jenkins.io/doc/developer/plugin-development/choosing-jenkins-baseline/ -->
                     <jenkins.baseline>2.479</jenkins.baseline>
-                    <jenkins.version>${jenkins.baseline}.1</jenkins.version>
+                    <jenkins.version>${jenkins.baseline}.3</jenkins.version>
                   </properties>
                   <dependencyManagement>
                     <dependencies>
