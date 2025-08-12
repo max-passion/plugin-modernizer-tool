43bdb96f1b9d241fadeb216def5e357e88044316
max-passion <liam46funkvzo@hotmail.com>
2025-08-12 23:43:19 +0530

    Add recipe MigrateCommonsLang2ToLang3AndCommonText to UpgradeNextMajorParentVersion, UpgradeToRecommendCoreVersion, UpgradeToLatestJava11CoreVersion, UpgradeToLatestJava8CoreVersion and add pr body and title.

--- plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/DeclarativeRecipesTest.java
+++ plugin-modernizer-core/src/test/java/io/jenkins/tools/pluginmodernizer/core/recipes/DeclarativeRecipesTest.java
@@ -2617,7 +2617,60 @@ public class DeclarativeRecipesTest implements RewriteTest {
                                 .formatted(
                                         Settings.getJenkinsParentVersion(),
                                         Settings.getJenkinsTestHarnessVersion(),
-                                        Settings.getBomVersion())));
+                                        Settings.getBomVersion())),
+
+                // language=java
+                java(
+                        """
+                package org.apache.commons.lang;
+                public class StringEscapeUtils {
+                    public static String escapeHtml(String input) {
+                        return input;
+                    }
+                }
+                """,
+                        """
+                package org.apache.commons.text;
+                public class StringEscapeUtils {
+                    public static String escapeHtml4(String input) {
+                        return input;
+                    }
+                }
+                """),
+                // language=java
+                java(
+                        """
+                package org.apache.commons.lang;
+                public class StringUtils {}
+                """,
+                        """
+                package org.apache.commons.lang3;
+                public class StringUtils {}
+                """),
+                // language=java
+                java(
+                        """
+                import org.apache.commons.lang.StringEscapeUtils;
+                import org.apache.commons.lang.StringUtils;
+
+                class MyComponent {
+                    public String getHtml() {
+                        String unsafeInput = "<script>alert('xss')</script>";
+                        return StringEscapeUtils.escapeHtml(unsafeInput);
+                    }
+                }
+                """,
+                        """
+                import org.apache.commons.lang3.StringUtils;
+                import org.apache.commons.text.StringEscapeUtils;
+
+                class MyComponent {
+                    public String getHtml() {
+                        String unsafeInput = "<script>alert('xss')</script>";
+                        return StringEscapeUtils.escapeHtml4(unsafeInput);
+                    }
+                }
+                """));
     }
 
     @Test
